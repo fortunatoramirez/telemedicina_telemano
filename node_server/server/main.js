434437919ebb284cf95f6ebfcd19586810409383
bioMarkez <66761563+bioMarkez@users.noreply.github.com>
2020-06-11 21:13:56 -0700

    msg angulo

--- node_server/server/main.js
+++ node_server/server/main.js
@@ -3,18 +3,18 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-/*
 app.use(express.static('public'));
 app.get('/hello', function(req, res){
 	res.status(200).send("Hola mundo");
 });
-*/
 
 io.on('connection',function(socket){
-	socket.on('nuevo_mensaje', function(msg){
-		//console.log(msg);
+	
+	socket.on('angulo', function(msg){
+		console.log(msg)
 		io.sockets.emit('angulo',msg);
 	});
+
 });
 
 server.listen(5001, function(){
