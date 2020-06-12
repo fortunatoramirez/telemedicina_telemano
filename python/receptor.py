ac47bd4527d511dd07f8cd600bdeb425611ce8d2
bioMarkez <66761563+bioMarkez@users.noreply.github.com>
2020-06-11 20:22:28 -0700

    etiqueta_angulo

--- python/receptor.py
+++ python/receptor.py
@@ -3,16 +3,16 @@ import serial
 import time
 
 print("Comenzando...")
-socketIO = SocketIO('201.122.174.201', 5001)
+socketIO = SocketIO('201.174.122.203', 5001)
 print("Conectado al servidor.")
 
-arduino=serial.Serial('/dev/ttyACM0',9600, timeout = 3.0)
+#arduino=serial.Serial('/dev/ttyACM0',9600, timeout = 3.0)
 
 def response(*args):
     print(args[0])
-    arduino.write((args[0]).encode())
+    #arduino.write((args[0]).encode())
 
-socketIO.on("nuevo_mensaje", response)
+socketIO.on("angulo", response)
 socketIO.wait()
 
 arduino.close()
