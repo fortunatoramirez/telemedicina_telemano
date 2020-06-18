07c0b0d1d3775d7c59a677245e48ee363929d100
bioMarkez <66761563+bioMarkez@users.noreply.github.com>
2020-06-17 21:39:12 -0700

    descomentadas las lineas para escribir en arduino

--- python/receptor.py
+++ python/receptor.py
@@ -6,11 +6,11 @@ print("Comenzando...")
 socketIO = SocketIO('201.174.122.203', 5001)
 print("Conectado al servidor.")
 
-#arduino=serial.Serial('/dev/ttyACM0',9600, timeout = 3.0)
+arduino=serial.Serial('/dev/ttyACM0',9600, timeout = 3.0)
 
 def response(*args):
     print(args[0])
-    #arduino.write((args[0]).encode())
+    arduino.write((args[0]).encode())
 
 socketIO.on("angulo", response)
 socketIO.wait()
